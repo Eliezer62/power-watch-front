155708c45eb22bc9e425cae13cc67f1020215452
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-03-26 22:16:57 -0300

    add menu component

---
+++ src/app/layout/menu/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: "menu-nav",
+    templateUrl:'./menu.component.html',
+    styleUrl:'./menu.component.css',
+    imports: [],
+})
+
+export class MenuComponent {
+
+}
