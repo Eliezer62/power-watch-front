1ceb2dc7b2d9c592801f647387d78c6dbe6310d6
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-04-21 16:46:37 -0300

    add menu

---
+++ src/components/ui/menu.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { FileOutlined, HomeOutlined, SettingOutlined, UserOutlined, WifiOutlined} from '@ant-design/icons';
+
+export default function Menu() {
+
+    return (
+        <div className="bg-primary h-[100vh] w-[60px] fixed top-0 left-0">
+            <img src='/logos/logo_dark.png' className="mt-1"/>
+            <nav className="flex justify-center items-center h-[100vh]">
+                <ul className="flex justify-center flex-col items-center gap-[1.5rem]">
+                    <li className="text-white text-2xl hover:text-[#68A6E0]"><HomeOutlined /></li>
+                    <li className="text-white text-2xl hover:text-[#68A6E0]"><FileOutlined /></li>
+                    <li className="text-white text-2xl hover:text-[#68A6E0]"><WifiOutlined /></li>
+                    <li className="text-white text-2xl hover:text-[#68A6E0]"><UserOutlined /></li>
+                    <li className="text-white text-2xl hover:text-[#68A6E0]"><SettingOutlined /></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
