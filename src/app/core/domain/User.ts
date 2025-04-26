00847b1768c274929e65291f10087724be17acd9
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-04-26 16:50:43 -0300

    transform to class

--- src/app/core/domain/User.ts
+++ src/app/core/domain/User.ts
@@ -1,9 +1,55 @@
 import { UserRole } from "../enum/UserRole";
 
-export interface User {
-    id: string,
-    name: string,
-    email: string,
-    password: string,
-    role: UserRole
+export default class User {
+    #id: string;
+    #name: string;
+    #email: string;
+    #role: UserRole;
+
+    constructor(id: string, name: string, email: string, role: UserRole) {
+        this.#id = id;
+        this.#name = name;
+        this.#email = email;
+        this.#role = role;
+    }
+
+    getId() {
+        return this.#id;
+    }
+
+    getName() {
+        return this.#name;
+    }
+
+    getEmail() {
+        return this.#email;
+    }
+
+    getRole() {
+        return this.#role;
+    }
+
+    setId(id: string) {
+        this.#id = id;
+        return this;
+    }
+
+    setName(name: string) {
+        this.#name = name;
+        return this;
+    }
+
+    setEmail(email: string) {
+        this.#email = email;
+        return this;
+    }
+
+    setRole(role: UserRole) {
+        this.#role = role;
+        return this;
+    }
+
+    build() {
+        return this;
+    }
 }
