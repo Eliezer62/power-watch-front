7c78219a1073a8f65309591efc09baa201a6b0d3
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-04-26 15:05:11 -0300

    fix logo and add user redirect

--- src/components/ui/menu.tsx
+++ src/components/ui/menu.tsx
@@ -5,13 +5,13 @@ export default function Menu() {
 
     return (
         <div className="bg-primary dark:bg-neutral-900 h-[100vh] w-[60px] fixed top-0 left-0">
-            <img src='/logos/logo_dark.png' className="mt-1"/>
+            <img src='/logos/logo_dark.png' className="mt-[1rem]"/>
             <nav className="flex justify-center items-center h-[100vh]">
                 <ul className="flex justify-center flex-col items-center gap-[1.5rem]">
                     <li className="text-white text-2xl hover:text-[#68A6E0]"><HomeOutlined /></li>
                     <li className="text-white text-2xl hover:text-[#68A6E0]"><FileOutlined /></li>
                     <li className="text-white text-2xl hover:text-[#68A6E0]"><WifiOutlined /></li>
-                    <li className="text-white text-2xl hover:text-[#68A6E0]"><UserOutlined /></li>
+                    <li className="text-white text-2xl hover:text-[#68A6E0]" onClick={() => location.href = '/user'}><UserOutlined /></li>
                     <li className="text-white text-2xl hover:text-[#68A6E0]"><SettingOutlined /></li>
                 </ul>
             </nav>
