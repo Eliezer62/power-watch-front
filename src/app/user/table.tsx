c41683c18005dfda433a93d93218fb7e127feb5d
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-04-26 19:35:54 -0300

    add user fetch from api

--- src/app/user/table.tsx
+++ src/app/user/table.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import {
@@ -10,12 +11,32 @@ import {
     TableRow,
   } from "@/components/ui/table"
 
-import { EditOutlined, MinusCircleOutlined } from "@ant-design/icons"
+import { EditOutlined } from "@ant-design/icons"
 import { SearchIcon } from "lucide-react"
 import NewUser from "./new.user"
 import RemoveUser from "./remove.user"
+import { useEffect, useState } from "react"
+import { UserServiceFromAPI } from "../infra/services/UserServiceFromAPI"
+import User from '@/app/core/domain/User'
 
 export default function TableUser() {
+    const [users, setUsers] = useState<User[]>([]);
+    const [updateTime, setUpdateTime] = useState(0);
+
+    const service = new UserServiceFromAPI();
+
+    useEffect(() => {
+        const timeout = setInterval(() => {
+            service.findAll().then((resp) => setUsers(resp));
+            setUpdateTime(3000);
+        }, updateTime);
+
+        return () => {
+            clearInterval(timeout);
+        }
+    });
+    
+
     return (
         <div>
             <div className="flex w-[100vw] h-[75px] p-10 items-center flex-row-reverse gap-[24px]">
@@ -37,15 +58,19 @@ export default function TableUser() {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        <TableRow>
-                            <TableCell className="dark:text-white">Eliezer de Almeida</TableCell>
-                            <TableCell className="dark:text-white">example@email.com</TableCell>
-                            <TableCell className="dark:text-white">Administrador</TableCell>
-                            <TableCell className="flex gap-[0.5rem]">
-                                <Button className="border-2 border-primary bg-transparent text-primary hover:bg-gray-200 hover:border-gray-700"><EditOutlined /></Button>
-                                <RemoveUser user={{name:"Eliezer"}}/>
-                            </TableCell>
-                        </TableRow>
+                        {users.map((user, index) => {
+                            return (
+                                <TableRow key={index}>
+                                    <TableCell className="dark:text-white">{user.name}</TableCell>
+                                    <TableCell className="dark:text-white">{user.email}</TableCell>
+                                    <TableCell className="dark:text-white">{user.role}</TableCell>
+                                    <TableCell className="flex gap-[0.5rem]">
+                                        <Button className="border-2 border-primary bg-transparent text-primary hover:bg-gray-200 hover:border-gray-700"><EditOutlined /></Button>
+                                        <RemoveUser user={{name:user.name}}/>
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })}
                     </TableBody>
                 </Table>
             </div>
